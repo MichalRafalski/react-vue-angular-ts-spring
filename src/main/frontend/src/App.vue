ad75ed2842b3efcc88b5ea967fe52bfb2b3e00ab
Websitescity <websitescity@gmail.com>
2018-04-23 21:02:33 +0200

    Add Navbar component (in Bootstrap)

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <navbar msg="Vue-Ts"/>      
     </div>
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Navbar from '@/components/Navbar.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    Navbar,
+  },
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 @import "./assets/styles/custom-bootstrap.scss";
@@ -18,15 +29,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 56px;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+
 </style>
